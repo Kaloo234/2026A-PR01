d256cfdb27d0aca1f4dae8e9b0f62bb6cfe443bd
Kaloo234 <165850231+Kaloo234@users.noreply.github.com>
2026-09-17 16:23:19 -0400

    1.2

--- game.py
+++ game.py
@@ -40,13 +40,24 @@ def move_doodle():
     # TODO : Gérez les déplacements gauche/droite et mettez à jour
     # simultanément la direction et l'image du Doodle.
 
-
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        doodle_dict["x"] -= DOODLE_SPEED
+        doodle_dict["direction"] = "left"
+        doodle_dict["image"] = doodle_left_img
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        doodle_dict["x"] += DOODLE_SPEED
+        doodle_dict["direction"] = "right"
+        doodle_dict["image"] = doodle_right_img
 
     # TODO : Implémentez le Screen Wrap pour qu'une partie du Doodle puisse
     # sortir d'un côté avant de réapparaître de l'autre.
     # N'utilisez pas de dimensions numériques écrites directement.
 
-
+    # bord droit
+    if doodle_dict["x"] + DOODLE_WIDTH/2 > SCREEN_WIDTH:
+        doodle_dict["x"] = -DOODLE_WIDTH/2
+    if doodle_dict["x"] < -DOODLE_WIDTH/2:
+            doodle_dict["x"] = SCREEN_WIDTH - DOODLE_WIDTH/2
 
     return
 
